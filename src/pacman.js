75bb025cdb3c58a337e5060cddfacff07777f945
Aliffiqri Agwar <37689921+SorrowInRain@users.noreply.github.com>
2019-05-14 06:36:05 +0700

    Fixing movement problem

--- src/pacman.js
+++ src/pacman.js
@@ -13,31 +13,19 @@ export default class Pacman{
     }
 
     moveUp(){
-        if(this.game.paths[(this.y-1) * this.game.dimension + this.x].movable == 1)
-            this.speedY = -1;
-        else
-            this.speedY = 0;
+        this.speedY = -1;
     }
 
     moveLeft(){
-        if(this.game.paths[(this.y) * this.game.dimension + this.x-1].movable == 1)
-            this.speedX = -1;
-        else
-            this.speedX = 0;
+        this.speedX = -1;
     }
 
     moveRight(){
-        if(this.game.paths[(this.y) * this.game.dimension + this.x+1].movable == 1)
-            this.speedX = 1;
-        else
-            this.speedX = 0;
+        this.speedX = 1;
     }
 
     moveDown(){
-        if(this.game.paths[(this.y+1) * this.game.dimension + this.x].movable == 1)
-            this.speedY = 1;
-        else
-            this.speedY = 0;
+        this.speedY = 1;
     }
 
     stopX(){
@@ -59,8 +47,21 @@ export default class Pacman{
     }
 
     update(deltaTime){
-        this.x += this.speedX;
-        this.y += this.speedY;
+        if(this.game.paths[(this.y-1) * this.game.dimension + this.x].movable == 1 && this.speedY < 0){
+			this.y += this.speedY;
+		}
+		
+		if(this.game.paths[(this.y) * this.game.dimension + this.x-1].movable == 1 && this.speedX < 0){
+			this.x += this.speedX;
+		}
+		
+		if(this.game.paths[(this.y+1) * this.game.dimension + this.x].movable == 1 && this.speedY > 0){
+			this.y += this.speedY;
+		}
+		
+		if(this.game.paths[(this.y) * this.game.dimension + this.x+1].movable == 1 && this.speedX > 0){
+			this.x += this.speedX;
+		}
         
         if(this.x <= 0){
             this.x = 0;
